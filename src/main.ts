dc19a6cffab7f5f0a2e92924f44a2c6e99fdf98a
Paolo Mariotti <paolo.mariotti@sourcesense.com>
2024-02-06 00:55:37 +0100

    un monaco inizializzato

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { invoke } from "@tauri-apps/api/tauri";
+import * as monaco from 'monaco-editor';
 
 let greetInputEl: HTMLInputElement | null;
 let greetMsgEl: HTMLElement | null;
@@ -19,4 +20,28 @@ window.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
     greet();
   });
+
+  self.MonacoEnvironment = {
+    getWorkerUrl: function (moduleId, label) {
+      if (label === 'json') {
+        return './json.worker.bundle.js';
+      }
+      if (label === 'css' || label === 'scss' || label === 'less') {
+        return './css.worker.bundle.js';
+      }
+      if (label === 'html' || label === 'handlebars' || label === 'razor') {
+        return './html.worker.bundle.js';
+      }
+      if (label === 'typescript' || label === 'javascript') {
+        return './ts.worker.bundle.js';
+      }
+      return './editor.worker.bundle.js';
+    }
+  };
+
+  monaco.editor.create(document.getElementById('monaco') as HTMLElement, {
+    value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
+    language: 'javascript'
+  });
 });
+
